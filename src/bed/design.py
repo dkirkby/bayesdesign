fa983f14205bd3274693c043b8ccb3115687e00a
David Kirkby <dkirkby@uci.edu>
2024-08-28 16:45:06 -0700

    Add ExperimentDesigner class with some tests

---
+++ src/bed/design.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+from bed.grid import Grid, GridStack
+
+
+class ExperimentDesigner:
+    """Brute force calculation of expected information gain using Grids to define the parameters, features, and designs."""
+
+    def __init__(self, parameters, features, designs, likelihood):
+        """Initialize an experiment designer.
+
+        Parameters
+        ----------
+        parameters : Grid
+            grid used to tabulate the parameter space
+        features : Grid
+            grid used to tabulate the feature space
+        designs : Grid
+            grid used to tabulate the design space
+        likelihood : numpy array
+            grid of likelihood probabilities P(D|theta,xi) over the stack
+            (parameters, features, designs). Must be normalized over features
+            for each point on the (parameters, designs) grid.
+        """
+        if likelihood.shape != features.shape + designs.shape + parameters.shape:
+            raise ValueError(
+                "Likelihood shape not compatible with [features,designs,parameters]"
+            )
+        self.parameters = parameters
+        self.features = features
+        self.designs = designs
+        self.likelihood = likelihood
+        with GridStack(self.features, self.designs, self.parameters):
+            assert np.allclose(self.features.sum(self.likelihood), 1)
+        # Pre-allocate an internal buffer the same size as the input likelihood
+        # that we use to avoid allocating any large arrays in the calculate method.
+        self._buffer = np.zeros_like(likelihood)
+        self.posterior = self._buffer
+        self._initialized = False
+
+    def calculateEIG(self, prior, debug=False):
+        """Calculate the expected information gain for all possible designs.
+
+        Calculates following outputs as attributes of this class:
+        - H0: prior entropy in bits
+        - IG: grid of information gains in bits over the stack (features, designs).
+        - marginal: grid of feature probabilities P(D|xi) marginalized over
+          posterior probabilities, defined on the stack (features, designs).
+          Normalized over features for each point in the (features, designs) grid.
+        - EIG: grid of expected information gain in bits over the designs grid.
+
+        Parameters
+        ----------
+        prior : numpy array
+            Grid of prior probabilities P(theta) over the parameters.
+            Must be normalized over parameters.
+        debug : bool
+            Cross-checked buffer calculations against simpler expressions.
+            Note that this uses more memory and will take longer.
+        """
+        self.prior = prior
+        assert np.allclose(self.prior.sum(), 1)
+        # Calculate prior entropy in bits
+        self.H0 = -self.parameters.sum(prior * np.log2(prior))
+        with GridStack(self.features, self.designs, self.parameters):
+
+            # Tabulate the marginal probability P(y|xi) by integrating P(y|theta,xi) P(theta) over theta.
+            # No explicit normalization is required since P(y|theta,xi) and P(theta) are already normalized.
+            self._buffer[:] = self.likelihood
+            self._buffer *= self.prior
+            self.marginal = self.parameters.sum(self._buffer)
+            if debug:
+                marginal = self.parameters.sum(self.likelihood * self.prior)
+                assert np.allclose(marginal, self.marginal), "marginal check failed"
+
+            # Tabulate the posterior P(theta|y,xi) by normalizing P(y|theta,xi) P(theta) over parameters.
+            post_norm = self.parameters.sum(self._buffer, keepdims=True)
+            self._buffer /= post_norm
+            if debug:
+                posterior = self.parameters.normalize(self.likelihood * self.prior)
+                assert np.allclose(posterior, self._buffer), "posterior check failed"
+
+            # Tabulate the information gain in bits IG = post * log2(post) + H0.
+            # Do the calculations in stages to avoid allocating any large temporary arrays.
+            np.log2(self._buffer, out=self._buffer)
+            self._buffer *= self.likelihood
+            self._buffer *= self.prior
+            self._buffer /= post_norm
+            self.IG = self.H0 + self.parameters.sum(self._buffer)
+            if debug:
+                IG = self.H0 + self.parameters.sum(posterior * np.log2(posterior))
+                assert np.allclose(IG, self.IG), "IG check failed"
+
+            # Leave the posterior in the buffer.
+            self._buffer[:] = self.likelihood
+            self._buffer *= self.prior
+            self._buffer /= post_norm
+
+        with GridStack(self.features, self.designs):
+            # Tabulate the expected information gain in bits as avg of IG(y,xi) with weights P(y|xi).
+            self.EIG = self.features.sum(self.marginal * self.IG)
+        self._initialized = True
+        return self.EIG
+
+    def calculateMarginalEIG(self, *nuisance_params):
+        """Calculate the EIG using a posterior that is marginalized over the specified nuisance parameters."""
+        if not self._initialized:
+            raise RuntimeError("Must call calculateEIG before calculateMarginalEIG")
+        # Calculate the marginal prior and its entropy.
+        prior = self.parameters.sum(
+            self.prior, axis_names=nuisance_params, keepdims=True
+        )
+        H0 = -self.parameters.sum(prior * np.log2(prior))
+        # Calculate the marginal posterior and the information gain.
+        with GridStack(self.features, self.designs, self.parameters):
+            post = self.parameters.sum(
+                self.posterior, axis_names=nuisance_params, keepdims=True
+            )
+            # Calculate the information gain for all possible designs and measurements
+            IG = H0 + self.parameters.sum(post * np.log2(post))
+            # Tabulate the expected information gain in bits.
+            EIG = self.features.sum(self.marginal * IG)
+        return EIG
+
+    def describe(self):
+        if not self._initialized:
+            print("Not initialized")
+            return
+        for name in ("designs", "features", "parameters"):
+            grid = self.__dict__[name]
+            print(f"GRID  {name:>12s} {repr(grid)}")
+        for name in ("prior", "likelihood", "posterior", "marginal", "IG", "EIG"):
+            array = self.__dict__[name]
+            print(
+                f"ARRAY {name:>12s} {repr(array.shape):22s} {array.nbytes/(1<<20):7.1f} Mb"
+            )
+
+    def get_posterior(self, **design_and_features):
+        """Return the posterior P(theta|D,xi) for the specified design and features."""
+        # TODO: Check that design and features are fully specified...
+
+        with GridStack(self.features, self.designs, self.parameters) as stack:
+            loc = stack.at(**design_and_features)
+            # Return a copy so that future changes to our buffer have no side effects.
+            return np.array(self.posterior[loc])
+
+    def update(self, **design_and_features):
+        post = self.get_posterior(**design_and_features)
+        return self.calculateEIG(post)
---
+++ tests/test_design.py
@@ -0,0 +1,39 @@
+import unittest
+
+import numpy as np
+
+from bed.grid import Grid, GridStack
+from bed.design import ExperimentDesigner
+
+
+class TestDesign(unittest.TestCase):
+
+    def test_sine_wave(self, sigma_y=0.1):
+        designs = Grid(t_obs=np.linspace(0, 5, 51))
+        features = Grid(y_obs=np.linspace(-1.25, 1.25, 100))
+        params = Grid(amplitude=1, frequency=np.linspace(0.2, 2.0, 181), offset=0)
+
+        with GridStack(features, designs, params):
+            y_mean = params.amplitude * np.sin(
+                params.frequency * (designs.t_obs - params.offset)
+            )
+            y_diff = features.y_obs - y_mean
+            likelihood = np.exp(-0.5 * (y_diff / sigma_y) ** 2)
+            features.normalize(likelihood)
+        self.assertEqual(likelihood.shape, (100, 51, 1, 181, 1))
+        self.assertAlmostEqual(likelihood.max(), 0.10114700589838203)
+
+        designer = ExperimentDesigner(params, features, designs, likelihood)
+
+        prior = np.ones(params.shape)
+        params.normalize(prior)
+        self.assertAlmostEqual(prior.sum(), 1)
+
+        designer.calculateEIG(prior)
+        self.assertEqual(designer.posterior.shape, likelihood.shape)
+        self.assertEqual(designer.marginal.shape, (100, 51))
+        self.assertEqual(designer.IG.shape, (100, 51))
+        self.assertEqual(designer.EIG.shape, (51,))
+
+        self.assertAlmostEqual(designer.EIG.min(), 0)
+        self.assertAlmostEqual(designer.EIG.max(), 2.4501367058730814)
