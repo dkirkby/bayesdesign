d1f6a3e63950625224090cf714b9a415e71e2c52
David Kirkby <dkirkby@uci.edu>
2024-09-02 07:17:34 -0700

    add constrained grid tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -17,7 +17,7 @@ class TestGrid(unittest.TestCase):
         self.assertEqual(grid.names, ("x", "y"))
         self.assertTrue(np.array_equal(grid.x, [[0], [1], [2]]))
 
-    def test_ctor_keep(self):
+    def test_ctor_constraint(self):
         grid = Grid(
             x=np.arange(3),
             y=np.arange(4),
@@ -29,6 +29,28 @@ class TestGrid(unittest.TestCase):
             np.array_equal(grid.y, np.array([[0], [1], [2], [0], [1], [0]]))
         )
 
+    def test_ctor_constraint_invalid(self):
+        with self.assertRaises(ValueError):
+            Grid(x=[1, 2, 3], y=[4, 5], z=[6, 7, 8], constraint=lambda a, b: a < b)
+
+    def test_expand(self):
+        g1 = Grid(x=[1, 2, 3], u=[4, 5], y=1, v=[6, 7, 8, 9])
+        g2 = Grid(
+            x=[1, 2, 3],
+            u=[4, 5],
+            y=1,
+            v=[6, 7, 8, 9],
+            constraint=lambda u, v: (u + v) % 2,
+        )
+        z1 = g1.x * ((g1.u + g1.v) % 2) ** g1.y
+        z2 = g2.x * ((g2.u + g2.v) % 2) ** g2.y
+        z1e = g1.expand(z1)
+        z2e = g2.expand(z2)
+        assert z1e is z1
+        nonzero = z1 != 0
+        assert np.array_equal(z1[nonzero], z2e[nonzero])
+        assert np.all(np.isnan(z2e[~nonzero]))
+
     def test_sum(self):
         grid = Grid(x=np.arange(3), y=np.arange(4))
         self.assertEqual(grid.sum(np.ones(grid.shape)), 12)
@@ -39,6 +61,19 @@ class TestGrid(unittest.TestCase):
         self.assertTrue(np.array_equal(partial, np.full((4,), 3)))
         full = grid.sum(np.ones(grid.shape), axis_names=("x", "y"))
         self.assertEqual(full, 12)
+
+    def test_sum_constraint(self):
+        g1 = Grid(x=[1, 2, 3], u=[4, 5], y=1, v=[6, 7, 8, 9])
+        g2 = Grid(
+            x=[1, 2, 3],
+            u=[4, 5],
+            y=1,
+            v=[6, 7, 8, 9],
+            constraint=lambda u, v: (u + v) % 2,
+        )
+        z1 = g1.x * ((g1.u + g1.v) % 2) ** g1.y
+        z2 = g2.x * ((g2.u + g2.v) % 2) ** g2.y
+        assert g1.sum(z1) == g2.sum(z2)
 
     def test_sum_invalid(self):
         grid = Grid(x=np.arange(3), y=np.arange(4))
